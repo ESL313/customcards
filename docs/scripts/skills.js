1c2994b025311e8d39f7a97c948c8ce11834128f
ESL313 <76422006+ESL313@users.noreply.github.com>
2023-01-23 23:16:01 +0700

    font size change

--- docs/scripts/skills.js
+++ docs/scripts/skills.js
@@ -3,13 +3,13 @@ async function combatSkill(index, height) {
 
 	ctx.font = `${unit(30)}px genshin-font`;
 	const title = wrapText(skill.name, unit(350));
-	ctx.font = `${unit(25)}px genshin-font`;
+	ctx.font = `${unit(20)}px genshin-font`;
 	const description = wrapText(skill.description, unit(480));
 
 	ctx.globalAlpha = 0.2;
 	ctx.fillStyle = color.black;
 	ctx.beginPath();
-	ctx.roundRect(unit(560), unit(290) + height, unit(610), unit(Math.max(120, title.length * 40 + description.length * 35 + 40)), unit(20));
+	ctx.roundRect(unit(560), unit(290) + height, unit(610), unit(Math.max(120, title.length * 40 + description.length * 30 + 40)), unit(20));
 	ctx.fill();
 	ctx.globalAlpha = 1;
 
@@ -30,10 +30,10 @@ async function combatSkill(index, height) {
 	currentHeight += unit(30);
 
 	ctx.fillStyle = color.secondary;
-	ctx.font = `${unit(25)}px genshin-font`;
+	ctx.font = `${unit(20)}px genshin-font`;
 	for (const line of description) {
 		ctx.fillText(line, unit(680), unit(300) + height + currentHeight);
-		currentHeight += unit(35);
+		currentHeight += unit(30);
 	}
 
 	const skillImage = new Image();
@@ -64,13 +64,13 @@ async function specialSkill(index, height) {
 
 	ctx.font = `${unit(30)}px genshin-font`;
 	const title = wrapText(skill.name, unit(590));
-	ctx.font = `${unit(25)}px genshin-font`;
+	ctx.font = `${unit(20)}px genshin-font`;
 	const description = wrapText(skill.description, unit(590));
 
 	ctx.globalAlpha = 0.2;
 	ctx.fillStyle = color.black;
 	ctx.beginPath();
-	ctx.roundRect(unit(1240), unit(290) + height, unit(610), unit(title.length * 40 + description.length * 35 + 40), unit(20));
+	ctx.roundRect(unit(1240), unit(290) + height, unit(610), unit(title.length * 40 + description.length * 30 + 40), unit(20));
 	ctx.fill();
 	ctx.globalAlpha = 1;
 
@@ -91,10 +91,10 @@ async function specialSkill(index, height) {
 	currentHeight += unit(30);
 
 	ctx.fillStyle = color.secondary;
-	ctx.font = `${unit(25)}px genshin-font`;
+	ctx.font = `${unit(20)}px genshin-font`;
 	for (const line of description) {
 		ctx.fillText(line, unit(1250), unit(300) + height + currentHeight);
-		currentHeight += unit(35);
+		currentHeight += unit(30);
 	}
 
 	return currentHeight + unit(40);
